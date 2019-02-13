7fa10b597e1c7c23bf3524f9e805c3a07e633454
ramyaambika <43332585+ramyaambika@users.noreply.github.com>
2019-02-13 14:36:47 +0530

    Create +or-.c

---
+++ +or-.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+ printf("enter the value n":);
+ scanf("%d",&n);
+ if(n>0)
+  printf("%d is positive",n);
+ else if(n==0)
+  printf("%d is zero",n);
+ else if(n<0)
+  printf("%d is negative");
+ return 0;
+} 
